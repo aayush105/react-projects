9a75a62849f53c4075f63291af9e0a15a2cafe55
aayush105 <sthaaayush105@gmail.com>
2024-01-26 00:04:46 +0545

    crypto dashboard: sidenav ui add

---
+++ crypto-dashboard/src/Components/Sidenav.jsx
@@ -0,0 +1,90 @@
+import { Box, HStack, Heading, Icon, Stack, Text } from "@chakra-ui/react";
+import { MdOutlineDashboard } from "react-icons/md";
+import { GrTransaction } from "react-icons/gr";
+import { BiSupport } from "react-icons/bi";
+
+const Sidenav = () => {
+
+    const navLinks = [
+        {
+            icons: MdOutlineDashboard,
+            text: "Dashboard",
+            link: "/",
+        },
+        {
+            icons: GrTransaction,
+            text: "Transactions",
+            link: "/transactions",   
+        },
+    ];
+
+  return (
+    // this stack property === css property "display:flex" & "flex-direction:col"
+    <Stack justify="space-between" boxShadow="lg" maxW="256px" h="100vh" >
+        <Box>
+            <Heading 
+                textAlign="center"
+                as="h1" 
+                fontSize="20px"
+                pt="56px"
+            >
+                Crypto Dashboard
+            </Heading>
+            <Box mt="6" mx="3">
+                {
+                    navLinks.map((nav) =>
+                        (
+                            // this HStack === "display:flex" & "alignItem:center"
+                            // 3 === 12px as in chakraui 4*3 = 12, 1=4px
+                            <HStack 
+                                borderRadius="10px"
+                                key={nav.text} 
+                                py="3" 
+                                px="4" 
+                                _hover={{
+                                    bg: "#F3F3F7",
+                                    color:"#171717"
+                                }}
+                                color="#797E82"
+                            > 
+                                <Icon 
+                                    as={nav.icons}
+                                />
+                                <Text 
+                                    fontSize="14px"
+                                    fontWeight="medium" 
+                                >
+                                    {nav.text}
+                                </Text>
+                            </HStack>
+                        ))
+                }
+            </Box>
+        </Box>
+        <Box mt="6" mx="3" mb="6">
+            <HStack 
+                borderRadius="10px"                
+                py="3" 
+                px="4" 
+                _hover={{
+                    bg: "#F3F3F7",
+                    color:"#171717"
+                }}
+                color="#797E82"
+            > 
+                <Icon 
+                    as={BiSupport}
+                />
+                <Text 
+                    fontSize="14px"
+                    fontWeight="medium" 
+                >
+                    Support
+                </Text>
+            </HStack>
+        </Box>                
+    </Stack>
+  )
+}
+
+export default Sidenav;
---
+++ crypto-dashboard/src/pages/Dashboard/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Sidenav from '../../Components/Sidenav'
+
+const Dashboard = () => {
+  return (
+    <div>
+      <Sidenav />
+    </div>
+  )
+}
+
+export default Dashboard
