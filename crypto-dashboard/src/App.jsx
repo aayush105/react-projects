7253a269ab8f0748df044701f8a2c28e6e567a4d
aayush105 <sthaaayush105@gmail.com>
2024-02-08 21:29:25 +0545

    add:react router implement

--- crypto-dashboard/src/App.jsx
+++ crypto-dashboard/src/App.jsx
@@ -1,11 +1,28 @@
 import Dashboard from "./pages/Dashboard/Dashboard"
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Transaction from "./pages/Transaction/Transaction";
+import Support from "./pages/Support/Support";
 
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Dashboard />,
+    },
+    {
+      path: "/transaction",
+      element: <Transaction />,
+    },
+    {
+      path: "/support",
+      element: <Support />,
+    },
+  ]);
+
   return (
     <>
-      <Dashboard />
+      <RouterProvider router={router} />
     </>
   )
 }
--- crypto-dashboard/src/pages/Support/Support.jsx
+++ crypto-dashboard/src/pages/Support/Support.jsx
@@ -1,7 +1,9 @@
 import { Box } from "@chakra-ui/react";
 const Support = () => {
   return (
-    <></>
+    <>
+    <div>Support</div>
+    </>
   )
 }
 
