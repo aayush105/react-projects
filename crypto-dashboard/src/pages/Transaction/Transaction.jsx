77b062f1ab23a543f729c531efce168729e7589f
aayush105 <sthaaayush105@gmail.com>
2024-02-10 01:05:06 +0545

    transaction page complete

--- crypto-dashboard/src/pages/Transaction/Transaction.jsx
+++ crypto-dashboard/src/pages/Transaction/Transaction.jsx
@@ -1,32 +1,70 @@
-import {Button, Card, Flex, Icon, Tab, TabList, TabPanel, TabPanels, Tabs} from "@chakra-ui/react";
+import {Button, Card, Flex, HStack, Icon, Input, InputGroup, InputLeftElement, Tab, TabList, TabPanel, TabPanels, Tabs, Tag} from "@chakra-ui/react";
 import DashboardLayout from "../../Components/DashboardLayout";
 import { MdOutlineFileDownload } from "react-icons/md";
+import TransactionTable from "./components/TransactionTable";
+import { IoSearchCircleOutline } from "react-icons/io5";
 
 const Transaction = () => {
+
+  const tabs = [
+    {
+      name: "All",
+      count: 349,
+    },
+    {
+      name: "Deposit",
+      count: 114,
+    },
+    {
+      name: "Withdraw",
+      count: 55,
+    },
+    {
+      name: "Trade",
+      count: 50,
+    },
+  ];
+
   return (
-    <DashboardLayout>
+    <DashboardLayout title="Transactions">
       <Flex
         justify="end" mt="6" mb="3"
       >
         <Button leftIcon={<Icon as={MdOutlineFileDownload} />}>Export Csv</Button>
       </Flex>
-      <Card>
+      <Card borderRadius="1rem">
         <Tabs>
-          <TabList>
-            <Tab>One</Tab>
-            <Tab>Two</Tab>
-            <Tab>Three</Tab>
+          <TabList pt="4" display="flex" w="full" justifyContent="space-between">
+            <HStack>
+              {
+                tabs.map((tab)=> (
+                  <Tab key={tab.name} display="flex" gap="2" pb="4">
+                    {tab.name} <Tag colorScheme="gray" borderRadius="full">{tab.count}</Tag>
+                  </Tab>
+                ))
+              }
+            </HStack>
+
+            <InputGroup maxW="200px" pr="6">
+              <InputLeftElement pointerEvents='none'>
+              <Icon fontSize="25px" as={IoSearchCircleOutline} />
+              </InputLeftElement>
+              <Input type='search' placeholder='Search.......' />
+            </InputGroup>
           </TabList>
 
           <TabPanels>
             <TabPanel>
-              <p>one!</p>
+              <TransactionTable />
             </TabPanel>
             <TabPanel>
-              <p>two!</p>
+              <TransactionTable />
             </TabPanel>
             <TabPanel>
-              <p>three!</p>
+              <TransactionTable />
+            </TabPanel>
+            <TabPanel>
+              <TransactionTable />
             </TabPanel>
           </TabPanels>
         </Tabs>
